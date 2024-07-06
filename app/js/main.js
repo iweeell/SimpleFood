ec71f6877b92b937195816e6baeab119ee5694f0
iweeell <mihailyukov06@gmail.com>
2024-07-06 22:22:57 +0200

    catalog responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 $(function () {
 
+  $('.catalog__select').styler();
+
   $('.catalog__price-input').ionRangeSlider({
     type: "double",
     onStart: function (data){
@@ -16,22 +18,26 @@ $(function () {
     $('.burger-menu').toggleClass('burger-menu--active');
   })
 
+  $('.filter-btn, .filter-btn__close, .catalog__categories-link').on('click', function () {
+    $('.catalog__filter').toggleClass('catalog__filter--active');
+  })
+
   $('.coments__content').slick({
     dots: true,
     prevArrow: $('.slick-prev'),
     nextArrow: $('.slick-next')
   });
 
-    $(window).on('load resize', function () {
-      if ($(window).width() < 1200) {
-        $('.popular__filter:not(.slick-initialized)').slick({
-          arrows: false,
-          variableWidth: true
-        });
-      } else {
-        $(".popular__filter.slick-initialized").slick("unslick");
-      }
-    });
+  $(window).on('load resize', function () {
+    if ($(window).width() < 1200) {
+      $('.popular__filter:not(.slick-initialized)').slick({
+        arrows: false,
+        variableWidth: true
+      });
+    } else {
+      $(".popular__filter.slick-initialized").slick("unslick");
+    }
+  });
 
   $(window).on('load resize', function () {
     if ($(window).width() < 768) {
@@ -41,6 +47,17 @@ $(function () {
       });
     } else {
       $(".restorant__content.slick-initialized").slick("unslick");
+    }
+  });
+
+  $(window).on('load resize', function () {
+    if ($(window).width() < 768) {
+      $('.promo__content:not(.slick-initialized)').slick({
+        arrows: false,
+        dots: true
+      });
+    } else {
+      $(".promo__content.slick-initialized").slick("unslick");
     }
   });
 
