991467977ab7096d2f43823deba3bba2e5e3e5fc
iweeell <mihailyukov06@gmail.com>
2024-07-14 23:02:39 +0200

    add adoptive

--- app/js/main.js
+++ app/js/main.js
@@ -21,8 +21,17 @@ $(function () {
 
   $('.product__slider').slick({
     variableWidth: true,
+    infinite: false,
     prevArrow: '<button type="button" class="slick-prev"><svg width="19" height="32" viewBox="0 0 19 32" xmlns="http://www.w3.org/2000/svg"><path d="M0.384665 15.2262L16.398 0.839919C16.9234 0.324519 17.7756 0.324519 18.3011 0.839919C18.8266 1.35528 18.8266 2.19151 18.3011 2.70691L3.25336 16.2267L18.2998 29.7465C18.8253 30.2619 18.8253 31.0981 18.2998 31.6135C17.7743 32.1288 16.9221 32.1288 16.3967 31.6135L0.38333 17.2273C0.103261 16.9525 -0.0168143 16.5883 0.00189296 16.228C-0.0155212 15.8665 0.104514 15.5023 0.384665 15.2262Z" fill="#363853" /></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg width="19" height="32" viewBox="0 0 19 32" xmlns="http://www.w3.org/2000/svg"><path d="M18.3105 14.7728L2.29726 0.38655C1.77178 -0.12885 0.919578 -0.12885 0.394105 0.38655C-0.131368 0.90191 -0.131369 1.73814 0.394104 2.25354L15.4418 15.7734L0.395433 29.2931C-0.130041 29.8085 -0.130041 30.6447 0.395432 31.1601C0.920905 31.6755 1.77311 31.6755 2.29854 31.1601L18.3119 16.7739C18.5919 16.4991 18.712 16.1349 18.6933 15.7747C18.7107 15.4131 18.5907 15.0489 18.3105 14.7728Z" /></svg></button>',
+    responsive: [
+      {
+        breakpoint: 577,
+        settings: {
+          arrows: false,
+        }
+      }
+    ]
   });
 
   $('.star').rateYo({
@@ -61,7 +70,8 @@ $(function () {
     if ($(window).width() < 1200) {
       $('.popular__filter:not(.slick-initialized)').slick({
         arrows: false,
-        variableWidth: true
+        variableWidth: true,
+        infinite: false,
       });
     } else {
       $(".popular__filter.slick-initialized").slick("unslick");
@@ -87,6 +97,19 @@ $(function () {
       });
     } else {
       $(".promo__content.slick-initialized").slick("unslick");
+    }
+  });
+
+  $(window).on('load resize', function () {
+    if ($(window).width() < 576) {
+      $('.product-tabs__top-list:not(.slick-initialized)').slick({
+        arrows: false,
+        infinite: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      });
+    } else {
+      $(".product-tabs__top-list.slick-initialized").slick("unslick");
     }
   });
 
